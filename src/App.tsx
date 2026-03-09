2cfa03a2c24859e3149837994b1d4f82d10b4393
Nguyen Dang Phuc <dangphucnguyen20112005@gmail.com>
2026-03-09 19:29:28 +0700

    feat: update App component to include a button and adjust tsconfig paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Button } from "@/components/ui/button"
+
 
 
 function App() {
   return (
     <h1 className='text-amber-100'>
+      <Button size="lg">
+        Click me
+      </Button>
       Hello World!
     </h1>
   )
